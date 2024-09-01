7ab25c45348679ba8a79e29ba426e2ccce5ba5f0
Gigached <smallgigached@gmail.com>
2024-09-01 17:57:56 +0500

    validation

--- sign-up/sign-up.js
+++ sign-up/sign-up.js
@@ -1,19 +1,29 @@
 const container = document.querySelector(".container");
-const signUpTransitToSignUpToReg = document.querySelector(".sign-up_toggle_btn");
+const signUpTransitToSignUpToReg = document.querySelector(
+  ".sign-up_toggle_btn"
+);
 const SignUpButton = document.querySelector(".sign-up_button");
 
-const SignInTransitToSignInToAuth = document.querySelector(".sign-in_toggle_btn");
+const SignInTransitToSignInToAuth = document.querySelector(
+  ".sign-in_toggle_btn"
+);
 const SignInButton = document.querySelector(".sign-in_button");
-const SignInToEmail= document.querySelector(".sign-in_email");
+const SignInToEmail = document.querySelector(".sign-in_email");
 const SignInToPassword = document.querySelector(".sign-in_password");
 
 const SignUpToName = document.querySelector(".sign-up_name");
 const SignUpToEmail = document.querySelector(".sign-up_email");
 const SignUpToPassword = document.querySelector(".sign-up_password");
+
+const SignUpToRepeatToPassword = document.querySelector('.sign-up_repeat_password')
 const SignUpToAdaptive = document.querySelector(".sign-up_remove");
 const SignInToAdaptive = document.querySelector(".sing-in_remove");
-const serverLink = 'https://todo.s7b0t4-website-server.ru/';
-const pageLink = 'https://todo-smallgigached.netlify.app/';
+const serverLink = "https://todo.s7b0t4-website-server.ru/";
+const pageLink = "https://todo-smallgigached.netlify.app/";
+const titleToSignUp = document.querySelector('.titleToSignUp')
+const titleToSignIn =  document.querySelector('.title-to-sign-in')
+const error = document.querySelector('.error');
+const errortwo = document.querySelector('.errortwo')
 signUpTransitToSignUpToReg.addEventListener("click", () => {
   container.classList.add("active");
 });
@@ -24,11 +34,6 @@ SignInTransitToSignInToAuth.addEventListener("click", () => {
 SignInButton.addEventListener("click", SignInUser);
 SignUpButton.addEventListener("click", SignUpUser);
 
-SignInButton.addEventListener('keypress', (e) => {
-  if (e.key === "Enter") {
-    SignInUser();
-  }
-})
 function SignInUser(e) {
   e.preventDefault();
   axios
@@ -36,23 +41,25 @@ function SignInUser(e) {
       email: SignInToEmail.value,
       password: SignInToPassword.value,
     })
-    .then (async(response) => {
-      await localStorage.clear()
-      await localStorage.setItem('token', response.data.token)
-      window.location = pageLink + 'home/home.html';
+    .then(async (response) => {
+      await localStorage.clear();
+      await localStorage.setItem("token", response.data.token);
+      window.location = pageLink + "home/home.html";
     })
     .catch((err) => {
-      console.log(err);
+      console.log(err.response.data.message);   
+      errortwo.style.display = 'flex'
+      titleToSignIn.style.display = 'none'
+      errortwo.textContent = err.response.data.message;
     });
-} 
-
-SignUpButton.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    SignUpUser();
-  }
-});
+}
 
 function SignUpUser(e) {
+  if(SignUpToName.value == '') {
+    error.textContent = 'введите имя';
+    titleToSignUp.style.display = 'none'
+    controller.abort()
+  }
   e.preventDefault();
   axios
     .post(serverLink + "auth/signUp", {
@@ -60,16 +67,18 @@ function SignUpUser(e) {
       email: SignUpToEmail.value,
       password: SignUpToPassword.value,
     })
-    .then(async(response) => {
-      await localStorage.clear()
-      await localStorage.setItem('token', response.data.token)
+    .then(async (response) => {
+      await localStorage.clear();
+      await localStorage.setItem("token", response.data.token);
       if (response.data.id) {
         window.location = pageLink + `home/home.html`;
       }
-      console.error('Не верный аккаунт');
     })
-    .catch(function (error) {
-      console.log(error);
+    .catch((err) => {
+      console.log(err.response.data.message);   
+      error.style.display = 'flex'
+      titleToSignUp.style.display = 'none'
+      error.textContent = err.response.data.message;
     });
 }
 
